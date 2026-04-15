4314c1bdc364fc1f86e247045c16794e475bcbda
EnDerVaSiK <andreyka0309@gmail.com>
2026-04-15 17:33:34 +0300

    add logic to read text from input.txt

--- telephone-task/src/Main.java
+++ telephone-task/src/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Путь к нашему файлу с номерами телефонов
+        String inputFilePath = "telephone-task/input.txt";
+
+        try {
+            // Читаем весь текст из файла в одну строку
+            String text = new String(Files.readAllBytes(Paths.get(inputFilePath)));
+
+            System.out.println("Исходный текст из файла:");
+            System.out.println(text);
+
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при чтении файла: " + e.getMessage());
+        }
     }
 }
