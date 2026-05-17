99ebbfc7a6899b7d7d6647b1cf62fdf37c12a37e
EnDerVaSiK <andreyka0309@gmail.com>
2026-05-17 14:28:35 +0300

    downgrade java version to 17 for JaCoCo compatibility

---
+++ pr7-cucumber/src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        // Создаем наш калькулятор
+        Calculator calculator = new Calculator();
+        // Вызываем метод сложения (именно этот вызов даст нам 100% покрытие)
+        int result = calculator.add(5, 7);
+        // Проверяем, что 5+7 действительно 12
+        Assert.assertEquals(12, result);
+    }
+}
